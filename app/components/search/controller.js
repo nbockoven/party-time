9f439a45e3df168db118b7955a0e69337c723fc6
Nathaniel Bockoven <nbockoven@gmail.com>
2015-09-19 10:53:52 -0700

    sync

--- app/components/search/controller.js
+++ app/components/search/controller.js
@@ -3,186 +3,276 @@ party.controller('search', ['$scope', function( $scope ){
   $scope.candidates = [
     // REPUBLICAN
     {
-      'first_name': 'Jeb',
-      'last_name': 'Bush',
-      'party': 'Republican',
-      'experience': 'Former Governor of Florida',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Jeb_Bush_1786288.jpg',
-      'gender': 'Male'
+      first_name: 'Jeb',
+      last_name: 'Bush',
+      party: 'Republican',
+      experience: 'Former Governor of Florida',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Jeb_Bush_1786288.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Ben',
-      'last_name': 'Carson',
-      'party': 'Republican',
-      'experience': 'Former Neurosurgeon',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Ben_Carson_6036997.jpg',
-      'gender': 'Male'
+      first_name: 'Ben',
+      last_name: 'Carson',
+      party: 'Republican',
+      experience: 'Former Neurosurgeon',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Ben_Carson_6036997.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Donald',
-      'last_name': 'Trump',
-      'party': 'Republican',
-      'experience': 'President, Trump Organization',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Donald_Trump_6522122.jpg',
-      'gender': 'Male'
+      first_name: 'Donald',
+      last_name: 'Trump',
+      party: 'Republican',
+      experience: 'President, Trump Organization',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Donald_Trump_6522122.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Ted',
-      'last_name': 'Cruz',
-      'party': 'Republican',
-      'experience': 'U.S. Senator of Texas',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Ted_Cruz_1786303.jpg',
-      'gender': 'Male'
+      first_name: 'Ted',
+      last_name: 'Cruz',
+      party: 'Republican',
+      experience: 'U.S. Senator of Texas',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Ted_Cruz_1786303.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Marco',
-      'last_name': 'Rubio',
-      'party': 'Republican',
-      'experience': 'U.S. Senator of Florida',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Marco_Rubio_1786292.jpg',
-      'gender': 'Male'
+      first_name: 'Marco',
+      last_name: 'Rubio',
+      party: 'Republican',
+      experience: 'U.S. Senator of Florida',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Marco_Rubio_1786292.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Mike',
-      'last_name': 'Huckabee',
-      'party': 'Republican',
-      'experience': 'Former Governor of Arkansas',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Mike_Huckabee_1786294.jpg',
-      'gender': 'Male'
+      first_name: 'Mike',
+      last_name: 'Huckabee',
+      party: 'Republican',
+      experience: 'Former Governor of Arkansas',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Mike_Huckabee_1786294.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Rand',
-      'last_name': 'Paul',
-      'party': 'Republican',
-      'experience': 'U.S. Senator of Kentucky',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Rand_Paul_5645560.jpg',
-      'gender': 'Male'
+      first_name: 'Rand',
+      last_name: 'Paul',
+      party: 'Republican',
+      experience: 'U.S. Senator of Kentucky',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Rand_Paul_5645560.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Carly',
-      'last_name': 'Fiorina',
-      'party': 'Republican',
-      'experience': 'Former CEO of HP',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Carly_Fiorina_6036992.jpg',
-      'gender': 'Female'
+      first_name: 'Carly',
+      last_name: 'Fiorina',
+      party: 'Republican',
+      experience: 'Former CEO of HP',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Carly_Fiorina_6036992.jpg',
+      gender: 'Female'
     },
     {
-      'first_name': 'Scott',
-      'last_name': 'Walker',
-      'party': 'Republican',
-      'experience': 'Governor of Wisconsin',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Scott_Walker_1786302.jpg',
-      'gender': 'Male'
+      first_name: 'Scott',
+      last_name: 'Walker',
+      party: 'Republican',
+      experience: 'Governor of Wisconsin',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Scott_Walker_1786302.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'John',
-      'last_name': 'Kasich',
-      'party': 'Republican',
-      'experience': 'Governor of Ohio',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/John_Kasich_1786289.jpg',
-      'gender': 'Male'
+      first_name: 'John',
+      last_name: 'Kasich',
+      party: 'Republican',
+      experience: 'Governor of Ohio',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/John_Kasich_1786289.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Chris',
-      'last_name': 'Christie',
-      'party': 'Republican',
-      'experience': 'Governor of New Jersey',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Chris_Christie_1786282.jpg',
-      'gender': 'Male'
+      first_name: 'Chris',
+      last_name: 'Christie',
+      party: 'Republican',
+      experience: 'Governor of New Jersey',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Chris_Christie_1786282.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Rick',
-      'last_name': 'Santorum',
-      'party': 'Republican',
-      'experience': 'Former U.S. Senator of Pennsylvania',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Rick_Santorum_1786299.jpg',
-      'gender': 'Male'
+      first_name: 'Rick',
+      last_name: 'Santorum',
+      party: 'Republican',
+      experience: 'Former U.S. Senator of Pennsylvania',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Rick_Santorum_1786299.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Bobby',
-      'last_name': 'Jindal',
-      'party': 'Republican',
-      'experience': 'Governor of Louisiana',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Bobby_Jindal_1786297.jpg',
-      'gender': 'Male'
+      first_name: 'Bobby',
+      last_name: 'Jindal',
+      party: 'Republican',
+      experience: 'Governor of Louisiana',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/Bobby_Jindal_1786297.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Lindsey',
-      'last_name': 'Graham',
-      'party': 'Republican',
-      'experience': 'U.S. Senator of South Carolina',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Lindsey_Graham_6387899.jpg',
-      'gender': 'Male'
+      first_name: 'Lindsey',
+      last_name: 'Graham',
+      party: 'Republican',
+      experience: 'U.S. Senator of South Carolina',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Lindsey_Graham_6387899.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'George',
-      'last_name': 'Pataki',
-      'party': 'Republican',
-      'experience': 'Former Governor of New York',
-      'image': 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/George_Pataki_6502634.jpg',
-      'gender': 'Male'
+      first_name: 'George',
+      last_name: 'Pataki',
+      party: 'Republican',
+      experience: 'Former Governor of New York',
+      image: 'https://s2.graphiq.com/sites/default/files/980/media/images/t2/George_Pataki_6502634.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Jim',
-      'last_name': 'Gilmore',
-      'party': 'Republican',
-      'experience': 'Former Governor of Virginia',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Jim_Gilmore_6638843.jpg',
-      'gender': 'Male'
+      first_name: 'Jim',
+      last_name: 'Gilmore',
+      party: 'Republican',
+      experience: 'Former Governor of Virginia',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Jim_Gilmore_6638843.jpg',
+      gender: 'Male'
     },
 
     // DEMOCRATIC
     {
-      'first_name': 'Hillary',
-      'last_name': 'Clinton',
-      'party': 'Democratic',
-      'experience': 'Former Secretary of State',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Hillary_Clinton_1786283.jpg',
-      'gender': 'Female'
+      first_name: 'Hillary',
+      last_name: 'Clinton',
+      party: 'Democratic',
+      experience: 'Former Secretary of State',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Hillary_Clinton_1786283.jpg',
+      gender: 'Female'
     },
     {
-      'first_name': 'Bernie',
-      'last_name': 'Sanders',
-      'party': 'Democratic',
-      'experience': 'U.S. Senator of Vermont',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Bernie_Sanders_1786280.jpg',
-      'gender': 'Male'
+      first_name: 'Bernie',
+      last_name: 'Sanders',
+      party: 'Democratic',
+      experience: 'U.S. Senator of Vermont',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Bernie_Sanders_1786280.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Martin',
-      'last_name': 'O\'Malley',
-      'party': 'Democratic',
-      'experience': 'Former Governor of Maryland',
-      'image': 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Martin_OMalley_1786293.jpg',
-      'gender': 'Male'
+      first_name: 'Martin',
+      last_name: 'O\'Malley',
+      party: 'Democratic',
+      experience: 'Former Governor of Maryland',
+      image: 'https://s.graphiq.com/sites/default/files/980/media/images/t2/Martin_OMalley_1786293.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Jim',
-      'last_name': 'Webb',
-      'party': 'Democratic',
-      'experience': 'Former U.S. Senator of Virginia',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Jim_Webb_6387898.jpg',
-      'gender': 'Male'
+      first_name: 'Jim',
+      last_name: 'Webb',
+      party: 'Democratic',
+      experience: 'Former U.S. Senator of Virginia',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Jim_Webb_6387898.jpg',
+      gender: 'Male'
     },
     {
-      'first_name': 'Lincoln',
-      'last_name': 'Chafee',
-      'party': 'Democratic',
-      'experience': 'Former Governor of Rhode Island',
-      'image': 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Lincoln_Chafee_6387900.jpg',
-      'gender': 'Male'
+      first_name: 'Lincoln',
+      last_name: 'Chafee',
+      party: 'Democratic',
+      experience: 'Former Governor of Rhode Island',
+      image: 'https://s3.graphiq.com/sites/default/files/980/media/images/t2/Lincoln_Chafee_6387900.jpg',
+      gender: 'Male'
     }
   ];
 
-  $scope.genders = { Female: true, Male: true };
+  $scope.useParties = {};
+  $scope.useGenders = {};
 
-  $scope.parties = { Democratic: true, Republican: true };
+  $scope.$watch(function(){
+    return {
+      candidates: $scope.candidates,
+      useParties: $scope.useParties,
+      useGenders: $scope.useGenders
+    }
+  }, function( value ){
+    var selected;
 
-  $scope.filterByGender = function( candidate ) {
-    return $scope.genders[candidate.gender];
-  }
+    $scope.partiesGroup = uniqueItems($scope.candidates, 'party');
 
-  $scope.filterByParty = function( candidate ){
-    return $scope.parties[candidate.party]
-  }
+    var filterAfterParties = [];
+
+    selected = false;
+
+    for( var j in $scope.candidates ){
+      var p = $scope.candidates[j];
+      for( var i in $scope.useParties ){
+        if( $scope.useParties[i] ){
+          selected = true;
+          if( i == p.party ){
+            filterAfterParties.push(p);
+            break;
+          }
+        }
+      }
+    }
+
+    if( !selected ){
+      filterAfterParties = $scope.candidates;
+    }
+
+    $scope.gendersGroup = uniqueItems( filterAfterParties, 'gender' );
+
+    var filterAfterGenders = [];
+
+    selected = false;
+
+    for( var j in filterAfterParties ){
+      var p = filterAfterParties[j];
+      for( var i in $scope.useGenders ){
+        if( $scope.useGenders[i] ){
+          selected = true;
+          if( i == p.gender ){
+            filterAfterGenders.push(p);
+            break;
+          }
+        }
+      }
+    }
+
+    if( !selected ){
+      filterAfterGenders = filterAfterParties;
+    }
+
+    $scope.filteredCandidates = filterAfterGenders;
+  }, true);
+
+  $scope.$watch('filtered', function( newValue ){
+    if( angular.isArray( newValue ) ){
+      console.log(newValue.length);
+    }
+  }, true);
+
+
 }]);
+
+
+party.filter('count', function(){
+  return function( collection, key ){
+    var out = "test";
+    for( var i = 0; i < collection.length; i++ ){
+      //console.log(collection[i].pants);
+      //var out = party.filter('filter')(collection[i].pants, "42", true);
+    }
+    return out;
+  }
+});
+
+party.filter('groupBy', function(){
+  return function ( collection, key ){
+    if( collection === null ) return;
+    return uniqueItems( collection, key );
+  }
+});
+
+
+var uniqueItems = function( data, key ){
+    var result = new Array();
+    for (var i = 0; i < data.length; i++) {
+        var value = data[i][key];
+
+        if (result.indexOf(value) == -1) {
+            result.push(value);
+        }
+
+    }
+    return result;
+};
