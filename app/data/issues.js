1bf0f705f450c4354e431675483f1ef90c856c1a
Nathaniel Bockoven <nbockoven@gmail.com>
2015-09-21 12:20:34 -0700

    add issues

--- app/data/issues.js
+++ app/data/issues.js
@@ -124,8 +124,143 @@ party.factory('issues', function(){
 
 /*
 
+ISSUES
+
+Budget:
+  (Boby Jindal) Pass Constitutional Amendment to balance the federal budget. Increase defense spending. Consider private accounts for Social Security.
+  (Scott Walker) Privatize some government services. Delay debt payments to balance the state budget.
+  (Jim Webb) Cut the budget by reviewing all programs. Support military funding.
+  (Donald Trump) Government must cut spending more.
+  (Jeb Bush) Oppose tax increases, but do not sign a pledge against them.
+  (Lindsey Graham) Increase revenue, potentially including taxes, to help balance the budget.
+
+Campaign Finance:
+  Reform campaign finance laws. Allow Congress to limit spending.
+
+Social Security:
+  (Chris Christie) Gradually raise retirement age.
+  (Donald Trump) Do not cut benefits. Grow the economy to save program.
+
+Medicare:
+  (Chris Christie) Increase costs for wealthier Americans.
+  (Donald Trump) Do not cut benefits. Grow the economy to save program.
+
+Climate Change:
+  (Boby Jindal) It is real. Humans are causing some amount, "but the real question is how much."
+  (Scott Walker) Unclear whether climate change is real or manmade. Stop EPA emissions regulation.
+  (Jim Webb) Limit EPA power to regulate emissions. Expand energy access. Build the Keystone Pipeline.
+  (Chris Christie) Climate change is real and at least partially man-made. Approve the Keystone Pipeline. Skeptical of cap and trade.
+  (Donald Trump) It is a hoax.
+  (Jeb Bush) Climate change is a real concern. It is not clear how much is caused by humans.
+  (Lindsey Graham) Climate change is real, man-made, and an economic and environmental problem.
+
 Education:
-  Common Core
+  (Boby Jindal) Get rid of Common Core. Roll back funding on state-support universities. Increase funding for community colleges.
+  (Scott Walker) Increase school choice. Repeal Common Core.
+  (Chris Christie) End Common Core. Waive No Child Left Behind rules for states.
+  (Jeb Bush) Implement Common Core. No Child Left Behind is flawed but should have been reauthorized.
+  (Lincoln Chafee) The federal government has a role in education standards. No Child Left Behind was worth trying, needed adjustments.
+  (Lindsey Graham) End Common Core. Take the federal government out of education policy.
+
+Government Power:
+  (Lincoln Chafee) Give government limited surveillance powers. Bulk data collection violates the Constitution.
+
+Guns:
+  (Boby Jindal) Protect gun access.
+  (Jim Webb) Gun ownership is an important right.
+  (Chris Christie) Do not ban assault weapons. Support some gun control. Focus on addressing mental health issues.
+  (Donald Trump) Limit restrictions on guns. Ban some assault weapons and extend the waiting period for purchase.
+  (Jeb Bush) Expand gun owners' rights.
+  (Lindsey Graham) Allow access to assault weapons and larger magazine clips. Limit access for the mentally ill.
+
+Marijuana:
+  (Jeb Bush) Each state should decide on legalization. Florida should not allow either recreational or medical marijuana to be legalized and widely sold.
+  (Lincoln Chafee) Decriminalize possession of small amounts. Watch how things progress in Colorado.
+
+Immigration:
+  (Boby Jindal) Secure the border. Then allow a path to citizenship for undocumented residents. Expand legal immigration.
+  (Scott Walker) Secure the border. No path to citizenship. Deportation is not the solution.
+  (Jim Webb) Allow a path to citizenship after the border is secure. Support the DREAM Act.
+  (Chris Christie) No path to citizenship for undocumented immigrants. Allow in-state tuition for undocumented students.
+  (Donald Trump) No path to citizenship for undocumented workers. Allow more European immigration and a legal status to those graduating from U.S. colleges.
+  (Jeb Bush) Create a legal status, not a path to citizenship, for undocumented residents. Limit family-related legal immigration.
+  (Lincoln Chafee) Give undocumented immigrants access to in-state tuition. Offer a path to citizenship. Federal, not state, officials should enforce immigration laws.
+  (Lindsey Graham) Secure the border, then allow a path to citizenship for some undocumented immigrants. Block the president’s executive actions.
+
+Affordable Healthcare Act:
+  (Boby Jindal) Repeal and replace it with a new system.
+  (Jim Webb) President Obama mishandled the process. The law could have been narrower.
+  (Chris Christie) Repeal and replace it. New Jersey accepted Medicaid expansion portion of the law.
+  (Donald Trump) Repeal it. Replace it.
+  (Jeb Bush) Replace with “market-oriented” alternative.
+  (Lincoln Chafee) Keep the Affordable Care Act. Move toward universal coverage.
+  (Lindsey Graham) Repeal it or defund it.
+
+Prison Reform:
+  (Jim Webb) Initiate sweeping reform of criminal justice and incarceration system.
+
+Abortions:
+  (Boby Jindal) Ban most abortions after 20 weeks and restrict abortion clinics.
+  (Scott Walker) Ban nearly all abortion after 20 weeks.
+  (Jim Webb) Allow abortion access.
+  (Chris Christie) Ban abortions after 20 weeks, with exceptions for rape, incest and life of the mother.
+  (Donald Trump) Abortion should be limited.
+  (Jeb Bush) Ban most abortions after 20 weeks.
+  (Lincoln Chafee) Allow access to abortion.
+  (Lindsey Graham) Ban most abortions at 20 weeks.
+
+Gay Marriage:
+  (Boby Jindal) Marriage is between a man and a woman. If the Supreme Court upholds same-sex marriage, pass a Constitutional Amendment to ban it.
+  (Scott Walker) Pass a Constitutional Amendment to overturn the Supreme Court on gay marriage.
+  (Jim Webb) Same-sex marriage should be legal. Government has no role in private matters.
+  (Chris Christie) Marriage should be between a man and a woman but the Supreme Court ruling is "the law of the land."
+  (Donald Trump) Marriage is between a man and a woman and should be defined state by state.
+  (Jeb Bush) Marriage is between a man and a woman. Not clear on how to react if Supreme Court rules otherwise.
+  (Lincoln Chafee) Legalize same-sex marriage.
+  (Lindsey Graham) The Supreme Court decision on gay marriage stands. Republicans should not push for a Constitutional amendment.
+
+Taxes:
+  (Boby Jindal) Eliminate state income taxes. Look to local governments to plug Louisiana’s budget hole.
+  (Scott Walker) Cut tax rates. Consider abolishing income tax.
+  (Jim Webb) Cut corporate taxes. Raise taxes on investments.
+  (Chris Christie) Reform the tax code. (three income tax rates with highest at 28%. corporate tax code from 35% to 25%)
+  (Donald Trump) End corporate taxes. Lower individual rates. Consider a one-time tax on the wealthy to pay down the debt.
+  (Lincoln Chafee) Reform tax codes by lowering rates and ending deductions. Limit the estate tax.
+  (Lindsey Graham) Move toward a flat tax. Increase revenue, potentially including taxes, to help balance the budget.
+
+Trade:
+  (Lindsey Graham) Approve the Trans-Pacific Partnership.
+
+Unions and Workers:
+  (Scott Walker) Limit union organizing. Establish “right to work” laws.
+
+Welfare and Entitlements:
+  (Scott Walker) Reduce government assistance. Require drug tests and job training in exchange for benefits.
+
+Iran and Israel:
+  (Boby Jindal) Current nuclear talks are a mistake. Increase support for Israel.
+  (Scott Walker) Reject any deal coming from current nuclear talks. Increase support to Israel.
+  (Jim Webb) End current negotiations with Iran.
+  (Chris Christie) End current negotiations with Iran. Strengthen ties with Israel.
+  (Donald Trump) Walk away from nuclear talks. Increase sanctions.
+  (Jeb Bush) Cancel any nuclear deal President Obama reaches with Iran. Strengthen ties with Israel.
+  (Lincoln Chafee) The U.S. should negotiate with Iran. President George W. Bush was right to support a two-state solution and “Road Map” for peace between Israel and Palestinians.
+  (Lindsey Graham) Block the current nuclear framework with Iran. Defend Israel’s actions in the West Bank.
+
+Islamic State and Iraq:
+  (Boby Jindal) Keep open the option of sending U.S. ground troops.
+  (Scott Walker) Go beyond airstrikes. Consider sending ground troops.
+  (Jim Webb) The U.S. needs a clear strategy. Unknown if he would send troops to fight Islamic State. Presidents should limit military action.
+  (Chris Christie) Willing to send U.S. troops. Invading Iraq was a mistake.
+  (Donald Trump) Send a limited number of combat troops on the ground.
+  (Jeb Bush) Embed U.S. trainers with Iraqi troops. No decision yet on whether to send more ground troops. Build new U.S. base in Anbar province.
+  (Lincoln Chafee) The 2003 invasion of Iraq was a mistake. To fight Islamic State, the U.S. must build alliances, including with Russia.
+  (Lindsey Graham) Send 10,000 U.S. troops to fight Islamic State. Increase air strikes. Work with Iran to stabilize Iraq.
+
+Russia:
+  (Jeb Bush) Consider putting U.S. troops in the region. Hold more robust military exercises.
+
+
 
 
 */
