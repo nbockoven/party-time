58e31dde48789dd6cd23bc8dfd08303d8859eb2e
Nathaniel Bockoven <nbockoven@gmail.com>
2015-09-21 15:07:29 -0700

    and more issues

--- app/data/issues.js
+++ app/data/issues.js
@@ -133,13 +133,27 @@ Budget:
   (Donald Trump) Government must cut spending more.
   (Jeb Bush) Oppose tax increases, but do not sign a pledge against them.
   (Lindsey Graham) Increase revenue, potentially including taxes, to help balance the budget.
+  (Martin O'Malley) Mix spending cuts with tax increases. Restructure pension plans.
+  (Martin O'Malley) Use tax increases to fund government programs. Raise the minimum wage. Strengthen union bargaining.
+  (Rick Santorum) Balance the budget. Cap the size of the federal government.
+  (Rick Santorum) Raise minimum wage. Simplify tax rates.
+  (Mike Huckabee) Balance the budget. Consider raising retirement age for new workers.
+  (Marco Rubio) Balance it. Prioritize defense.
+  (Rand Paul) Balance the budget. Slash spending, but raise defense.
+  (Ted Cruz) Supports a Constitutional amendment mandating that Congress pass a balanced budget.
 
 Campaign Finance:
-  Reform campaign finance laws. Allow Congress to limit spending.
+  (Martin O'Malley) Reform campaign finance laws. Allow Congress to limit spending.
+  (Marco Rubio) Limit corporate and interest-group spending in campaigns.
+
+Banks and Wall Street:
+  (Martin O'Malley) Separate commercial and investment banks. Increase penalties for financial crimes.
+  (Bernie Sanders) Break up big banks. Open up the Fed.
 
 Social Security:
   (Chris Christie) Gradually raise retirement age.
   (Donald Trump) Do not cut benefits. Grow the economy to save program.
+  (Mike Huckabee) Raise eligibility age for future beneficiaries. Consider cutting cost-of-living increases.
 
 Medicare:
   (Chris Christie) Increase costs for wealthier Americans.
@@ -153,6 +167,13 @@ Climate Change:
   (Donald Trump) It is a hoax.
   (Jeb Bush) Climate change is a real concern. It is not clear how much is caused by humans.
   (Lindsey Graham) Climate change is real, man-made, and an economic and environmental problem.
+  (Martin O'Malley) It is real and a “natural threat.” Government should regulate greenhouse gas emissions.
+  (Rick Santorum) Not caused by human activity. Allow more oil and gas drilling.
+  (Mike Huckabee) Scientific predictions are inaccurate. But no hard, definitive stance.
+  (Carly Fiorina) It is real and manmade. But government has limited ability to address it.
+  (Ben Carson) The climate change debate is “irrelevant.” Temperature change is cyclical.
+  (Bernie Sanders) Charge companies for carbon emissions
+  (Marco Rubio) It is real. It is not caused by man.
 
 Education:
   (Boby Jindal) Get rid of Common Core. Roll back funding on state-support universities. Increase funding for community colleges.
@@ -161,9 +182,20 @@ Education:
   (Jeb Bush) Implement Common Core. No Child Left Behind is flawed but should have been reauthorized.
   (Lincoln Chafee) The federal government has a role in education standards. No Child Left Behind was worth trying, needed adjustments.
   (Lindsey Graham) End Common Core. Take the federal government out of education policy.
+  (Rick Santorum) End Common Core. Keep federal government out of education policy.
+  (Mike Huckabee) States, not federal government, should set similar standards. Common Core was a good idea and has been misconstrued.
+  (Carly Fiorina) Set national standards but give local districts maximum control. No Child Left Behind was positive.
+  (Ben Carson) No federally-determined standards. We need more school choice.
+  (Bernie Sanders) Two years free tuition at state colleges. Reform student loans.
+  (Hillary Clinton) Against No Child Left Behind. Position unknown on Common Core.
+  (Ted Cruz) End Common Core
 
 Government Power:
   (Lincoln Chafee) Give government limited surveillance powers. Bulk data collection violates the Constitution.
+  (Rand Paul) Repeal the Patriot Act. End large-scale data collection.
+
+Government Size:
+  (Rand Paul) Government size: Shut down Department of Education and the TSA.
 
 Guns:
   (Boby Jindal) Protect gun access.
@@ -172,10 +204,23 @@ Guns:
   (Donald Trump) Limit restrictions on guns. Ban some assault weapons and extend the waiting period for purchase.
   (Jeb Bush) Expand gun owners' rights.
   (Lindsey Graham) Allow access to assault weapons and larger magazine clips. Limit access for the mentally ill.
+  (Martin O'Malley) Increase gun control. Ban dozens of assault weapons. Limit size of gun magazines. Require fingerprints to buy a handgun.
+  (Mike Huckabee) Allow concealed carry. Protect citizens who use deadly force.
+  (Carly Fiorina) Gun access is an important right. Do not ban assault weapons. Allow some on the No-Fly list to own guns.
+  (Ben Carson) Few limits on ownership except for the mentally ill or those convicted of violent crime.
+  (Bernie Sanders) A mixed approach. No federal handgun waiting period. Some protection for gun manufacturers. Ban assault weapons.
 
 Marijuana:
   (Jeb Bush) Each state should decide on legalization. Florida should not allow either recreational or medical marijuana to be legalized and widely sold.
   (Lincoln Chafee) Decriminalize possession of small amounts. Watch how things progress in Colorado.
+  (Ben Carson) There are some benefits to medical marijuana. Legalizing for recreational purpose raises concern.
+  (Hillary Clinton) "Wait and see" on overall legalization.
+
+NSA:
+  (Hillary Clinton) More transparency. Find the balance between security, privacy.
+
+Justice:
+  (Rand Paul) Allow more felons to vote. Reform sentencing.
 
 Immigration:
   (Boby Jindal) Secure the border. Then allow a path to citizenship for undocumented residents. Expand legal immigration.
@@ -186,6 +231,21 @@ Immigration:
   (Jeb Bush) Create a legal status, not a path to citizenship, for undocumented residents. Limit family-related legal immigration.
   (Lincoln Chafee) Give undocumented immigrants access to in-state tuition. Offer a path to citizenship. Federal, not state, officials should enforce immigration laws.
   (Lindsey Graham) Secure the border, then allow a path to citizenship for some undocumented immigrants. Block the president’s executive actions.
+  (Martin O'Malley) Create a path to citizenship for immigrants in the country illegally. Pass the DREAM Act. Allow undocumented students to pay in-state tuition rates.
+  (Rick Santorum) Block the DREAM Act and any pathway to citizenship for undocumented workers. Restrict legal immigration. Increase border security.
+  (Mike Huckabee) Require most immigrants here illegally to self-deport within four months. Waive deportation for those brought here as children.
+  (Carly Fiorina) Pass the DREAM Act. For other undocumented immigrants, a direct path to citizenship is unfair.
+  (Ben Carson) Allow undocumented residents access to a national guest worker program if they leave the country first.
+  (Bernie Sanders) Offer path to citizenship. Waive some deportations now.
+  (Marco Rubio) Work toward reform, piece by piece. First stop the flow of illegal immigration and then address those in the country now
+  (Hillary Clinton) President should waive deportation for some immigrants. Give undocumented residents a path to legal status.
+  (Rand Paul) Reform, with some requirements.
+  (Ted Cruz) Block any current effort that lets undocumented immigrants legally remain in the U.S.
+
+Internet:
+  (Carly Fiorina) Overturn net neutrality.
+  (Marco Rubio) Oppose net neutrality.
+  (Ted Cruz) Do not tax access to the Internet and block "net neutrality."
 
 Affordable Healthcare Act:
   (Boby Jindal) Repeal and replace it with a new system.
@@ -195,6 +255,15 @@ Affordable Healthcare Act:
   (Jeb Bush) Replace with “market-oriented” alternative.
   (Lincoln Chafee) Keep the Affordable Care Act. Move toward universal coverage.
   (Lindsey Graham) Repeal it or defund it.
+  (Martin O'Malley) Expand the Affordable Care Act. Move to an "all-payer" system.
+  (Rick Santorum) Repeal it. Set up alternate system with tax credits for low-income Americans and high-risk pools for those with previous illness.
+  (Mike Huckabee) Repeal the Affordable Care Act. Government should subsidize those facing "extraordinary" costs.
+  (Carly Fiorina) Repeal the Affordable Care Act. Increase health insurance competition.
+  (Ben Carson) Replace with health savings accounts.
+  (Bernie Sanders) Change to single-payer government-provided health care
+  (Marco Rubio) Repeal it. Replace it with tax credits and fewer regulations.
+  (Hillary Clinton) Keep it. Strengthen it. Tout it.
+  (Ted Cruz) Repeal it.
 
 Prison Reform:
   (Jim Webb) Initiate sweeping reform of criminal justice and incarceration system.
@@ -208,6 +277,18 @@ Abortions:
   (Jeb Bush) Ban most abortions after 20 weeks.
   (Lincoln Chafee) Allow access to abortion.
   (Lindsey Graham) Ban most abortions at 20 weeks.
+  (Martin O'Malley) Allow access to abortion.
+  (Rick Santorum) Ban all abortion, except when the life of the mother is in danger.
+  (Mike Huckabee) Ban all abortions, except where life of the mother is at risk.
+  (Carly Fiorina) Ban most abortions after 20 weeks. Overturn Roe v. Wade.
+  (Ben Carson) Ban most abortions after 20 weeks.
+  (Marco Rubio) Ban abortion after 20 weeks.
+  (Hillary Clinton) Abortion should be legal.
+  (Rand Paul) Limit abortion, but do not overturn Roe v. Wade.
+  (Ted Cruz) There should be strict limits on abortion.
+
+Capital Punishment:
+  (Martin O'Malley) Abolish capital punishment.
 
 Gay Marriage:
   (Boby Jindal) Marriage is between a man and a woman. If the Supreme Court upholds same-sex marriage, pass a Constitutional Amendment to ban it.
@@ -218,6 +299,15 @@ Gay Marriage:
   (Jeb Bush) Marriage is between a man and a woman. Not clear on how to react if Supreme Court rules otherwise.
   (Lincoln Chafee) Legalize same-sex marriage.
   (Lindsey Graham) The Supreme Court decision on gay marriage stands. Republicans should not push for a Constitutional amendment.
+  (Martin O'Malley) Legalize same-sex marriage.
+  (Rick Santorum) Pass a Constitutional amendment to reverse the Supreme Court decision on gay marriage.
+  (Mike Huckabee) Push for a Constitutional amendment to reverse the Supreme Court’s gay marriage decision.
+  (Carly Fiorina) The Supreme Court was wrong on gay marriage. No position yet on whether it is settled law.
+  (Ben Carson) The Supreme Court ruling on gay marriage is the law of the land.
+  (Marco Rubio) The Supreme Court decision on gay marriage is the law of the land. Marriage is between a man and a woman.
+  (Hillary Clinton) Should be legal.
+  (Rand Paul) The U.S. should consider taking marriage out of government's hands.
+  (Ted Cruz) Only the four states (Kentucky, Michigan, Ohio and Tennessee) specifically named in the Supreme Court decision on gay marriage must abide by the ruling. In general, states should be allowed to define "marriage."
 
 Taxes:
   (Boby Jindal) Eliminate state income taxes. Look to local governments to plug Louisiana’s budget hole.
@@ -227,15 +317,34 @@ Taxes:
   (Donald Trump) End corporate taxes. Lower individual rates. Consider a one-time tax on the wealthy to pay down the debt.
   (Lincoln Chafee) Reform tax codes by lowering rates and ending deductions. Limit the estate tax.
   (Lindsey Graham) Move toward a flat tax. Increase revenue, potentially including taxes, to help balance the budget.
+  (Martin O'Malley) Mix spending cuts with tax increases. Restructure pension plans.
+  (Martin O'Malley) Use tax increases to fund government programs. Raise the minimum wage. Strengthen union bargaining.
+  (Rick Santorum) Raise minimum wage. Simplify tax rates.
+  (Mike Huckabee) Eliminate income tax and the IRS. Replace it with national sales tax.
+  (Carly Fiorina) Simplify the tax code. Move to zero-based budgeting. Do not increase the gas tax.
+  (Ben Carson) Establish a flat tax. Eliminate the IRS.
+  (Bernie Sanders) Raise some taxes on the wealthy. Cut taxes for middle class.
+  (Marco Rubio) Cut corporate taxes to 25 percent. Simplify individual brackets. Add $2,500 child tax credit.
+  (Hillary Clinton) Consider closing loopholes and cutting middle class taxes.
+  (Rand Paul) Institute a flat tax of 17 percent or lower.
+  (Ted Cruz) Slash corporate tax rates to 15 percent. End some programs like the Export-Import bank and federal subsidies for renewable fuels.
+  (Ted Cruz) Move toward a flat tax and abolish the IRS.
 
 Trade:
   (Lindsey Graham) Approve the Trans-Pacific Partnership.
+  (Martin O'Malley) Block the Trans-Pacific Partnership.
 
 Unions and Workers:
   (Scott Walker) Limit union organizing. Establish “right to work” laws.
 
 Welfare and Entitlements:
   (Scott Walker) Reduce government assistance. Require drug tests and job training in exchange for benefits.
+
+Foreign Aid:
+  (Rand Paul) End it.
+
+Cuba:
+  (Marco Rubio) Block the Obama administration’s "normalization"
 
 Iran and Israel:
   (Boby Jindal) Current nuclear talks are a mistake. Increase support for Israel.
@@ -246,6 +355,16 @@ Iran and Israel:
   (Jeb Bush) Cancel any nuclear deal President Obama reaches with Iran. Strengthen ties with Israel.
   (Lincoln Chafee) The U.S. should negotiate with Iran. President George W. Bush was right to support a two-state solution and “Road Map” for peace between Israel and Palestinians.
   (Lindsey Graham) Block the current nuclear framework with Iran. Defend Israel’s actions in the West Bank.
+  (Martin O'Malley) Continue negotiations with Iran. Work for a two-state solution between Israel and Palestinians.
+  (Rick Santorum) Ramp up sanctions on Iran. Strengthen alliance with Israel.
+  (Mike Huckabee) Contested territory belongs to Israel. No to a two-state solution with Palestinians.
+  (Carly Fiorina) Support Israel by breaking off nuclear talks. Verify actions before Iran sanctions are lifted.
+  (Ben Carson) Congress must be involved in Iran deal. U.S. should listen to and consider Netanyahu’s words.
+  (Bernie Sanders) Supports current talks with Iran. Critical of Israeli Prime Minister Benjamin Netanyahu.
+  (Marco Rubio) Toughen sanctions. Scrap proposed nuclear deal.
+  (Hillary Clinton) Support framework for nuclear deal. Continue diplomacy efforts and some sanctions.  Work toward a two-state solution. Do not necessarily freeze settlement building.
+  (Rand Paul) Sanctions and diplomacy first
+  (Ted Cruz) Increase and toughen sanctions. End current nuclear talks until Congress approves the outlines of a deal.
 
 Islamic State and Iraq:
   (Boby Jindal) Keep open the option of sending U.S. ground troops.
@@ -256,6 +375,16 @@ Islamic State and Iraq:
   (Jeb Bush) Embed U.S. trainers with Iraqi troops. No decision yet on whether to send more ground troops. Build new U.S. base in Anbar province.
   (Lincoln Chafee) The 2003 invasion of Iraq was a mistake. To fight Islamic State, the U.S. must build alliances, including with Russia.
   (Lindsey Graham) Send 10,000 U.S. troops to fight Islamic State. Increase air strikes. Work with Iran to stabilize Iraq.
+  (Martin O'Malley) No specific stance yet. Congress should set clear parameters for any use of ground troops.
+  (Rick Santorum) More air strikes. Deploy 10,000 U.S. ground troops.
+  (Mike Huckabee) Nations in the Middle East region must do more to fight Islamic State, but the U.S. may have to put boots on the ground.
+  (Carly Fiorina) Arm the Kurds.
+  (Ben Carson) The U.S. should step up its efforts to destroy the Islamic State militant group. Do not “tie” the military’s hands.
+  (Bernie Sanders) Opposed the Iraq war. Calls for troop withdrawal as soon as possible (Afghanistan).
+  (Marco Rubio) Increase the president’s power to combat.
+  (Hillary Clinton) No boots on the ground. Use regional troops.
+  (Rand Paul) Arm the Kurds, declare war. Limited troops.
+  (Ted Cruz) Don’t send U.S. ground troops, yet.
 
 Russia:
   (Jeb Bush) Consider putting U.S. troops in the region. Hold more robust military exercises.
