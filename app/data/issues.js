17ce8e6fafcfa6bcaae0d0f54aa7a0b18d5f8edb
Nathaniel Bockoven <nbockoven@gmail.com>
2015-09-21 06:19:17 -0700

    add filtering functionality

---
+++ app/data/issues.js
@@ -0,0 +1,131 @@
+party.factory('issues', function(){
+  return [
+    {
+      title: 'Abortion',
+      description: 'The termination of a pregnancy by the removal or expulsion from the uterus of a fetus or embryo, resulting in or caused by its death.'
+    },
+    {
+      title: 'Affirmative Action',
+      description: 'The policy of favoring members of a disadvantaged group who suffer from discrimination within a culture.'
+    },
+    {
+      title: 'Animal Rights',
+      description: 'The idea that the most basic interests of animals should be afforded the same consideration as the similar interests of humans.'
+    },
+    {
+      title: 'Civil Rights',
+      description: ''
+    },
+    {
+      title: 'Death Penalty',
+      description: ''
+    },
+    {
+      title: 'Education',
+      description: ''
+    },
+    {
+      title: 'Firearms',
+      description: "Pro and anti firearm issues from assault weapons and the Violent Crime Control and Prevention Act to Gun Rights - GunSafe: Children, Guns and Smart Guns"
+    },
+    {
+      title: 'Free Trade',
+      description: "A system of trade policy that allows traders to act and transact without interference from government."
+    },
+    {
+      title: 'Health Care',
+      description: "The treatment and management of illnesses of the elderly, and the preservation of health through services offered by the medical, dental, complementary and alternative medicine, pharmaceutical, clinical sciences (in vitro diagnostics), nursing, and allied health professions."
+    },
+    {
+      title: 'Immigration',
+      description: "The introduction of new people into a habitat or population."
+    },
+    {
+      title: 'North American Free Trade Agreement (NAFTA)',
+      description: "An agreement signed by the governments of the United States, Canada, and Mexico creating a trilateral trade bloc in North America."
+    },
+    {
+      title: 'Patient Rights',
+      description: "A statement of the rights to which patients are entitled as recipients of medical care."
+    },
+    {
+      title: 'Race Relations',
+      description: "Racism Offers the origins and history of racism throughout the world and provides examples of specific types of discrimination."
+    },
+    {
+      title: 'Terrorism',
+      description: "The systematic use of terror especially as a means of coercion."
+    },
+    {
+      title: 'Universal Health Care',
+      description: "Health care coverage for all eligible residents of a political region and often covers medical, dental and mental health care."
+    },
+    {
+      title: 'War and Peace',
+      description: "War is a behavior pattern exhibited by many primate species including man, and also found in many ant species. Peace is a quality describing a society or a relationship that is operating harmoniously."
+    },
+    {
+      title: 'Welfare And Poverty',
+      description: "Welfare or welfare work consists of actions or procedures, especially on the part of governments and institutions, striving to promote the basic well-being of individuals in need."
+    },
+    {
+      title: 'Budget and Economy',
+      description: "Provide a forecast of revenues and expenditures i.e. construct a model of how our business might perform financially speaking if certain strategies, events and plans are carried out."
+    },
+    {
+      title: 'Child Support',
+      description: "A payment that a non-custodial parent makes as a contribution to the costs of raising her or his child. In the mid-1990s, as never before, child support became a topic of urgent U.S. national discussion."
+    },
+    {
+      title: 'Climate Change',
+      description: "A change in the statistical distribution of weather patterns when that change lasts for an extended period of time."
+    },
+    {
+      title: 'Don\'t Ask Don\'t Tell',
+      description: "The discriminatory Dont Ask, Dont Tell ban on gay and lesbian service members is officially in the dustbin of history."
+    },
+    {
+      title: 'Flat Taxes',
+      description: "A flat tax (short for flat rate tax) is a tax system with a constant tax rate."
+    },
+    {
+      title: 'Gay Rights',
+      description: "Lesbian, gay, bisexual, and transgender (LGBT) related laws vary greatly by country or territory � everything from legal recognition of same-sex marriage or other types of partnerships, to the death penalty as punishment for same-sex sexual activity or identity."
+    },
+    {
+      title: 'Homeland Security',
+      description: "An umbrella term for security efforts to protect the United States against perceived internal and external threats."
+    },
+    {
+      title: 'Infrastructure & Technology',
+      description: "he Information Technology Infrastructure Library (ITIL) is a set of concepts and practices for managing Information Technology (IT) services (ITSM), IT development and IT operations."
+    },
+    {
+      title: 'Medical Marijuana',
+      description: "Refers to the use of cannabis and its constituent cannabinoids, such as tetrahydrocannabinol (THC) and cannabidiol (CBD), as medical therapy to treat disease or alleviate symptoms."
+    },
+    {
+      title: 'Nuclear Energy & Weapons',
+      description: "A nuclear weapon is an explosive device that derives its destructive force from nuclear reactions, either fission or a combination of fission and fusion."
+    },
+    {
+      title: 'Stem Cell Research',
+      description: "Stem cells are cells found in most, if not all, multi-cellular organisms. They are characterized by the ability to renew themselves through mitotic cell division and differentiating into a diverse range of specialized cell types."
+    },
+    {
+      title: 'Internet Censorship',
+      description: 'The control or suppression of the publishing...'
+    },
+
+
+  ]
+});
+
+
+/*
+
+Education:
+  Common Core
+
+
+*/
