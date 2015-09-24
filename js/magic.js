a292248cd40b0b34a178d4611ba8fcd4353dea8d
Nathaniel Bockoven <nbockoven@gmail.com>
2015-09-24 12:10:34 -0700

    add mobile filter view

--- js/magic.js
+++ js/magic.js
@@ -1,8 +1,28 @@
 
-$(document).on('change', '#form-questionnaire input', function(){
+$(document)
+.on('change', '#form-questionnaire input', function(){
   setTimeout( slideCarousel, 500 );
+})
+.on('click', '[data-toggle="offcanvas"]', function(){
+  $('.row-offcanvas').toggleClass('active')
+})
+.ready(function(){
+  checkScreenSize();
 });
 
 function slideCarousel(){
   $('#form-questionnaire').carousel('next');
+}
+
+
+$(window).resize(function(){
+  checkScreenSize();
+});
+
+function checkScreenSize(){
+  // if screen is small, then apply .nav-justified to nav-pills
+  if( $(window).width() < 768  )
+    $('.jumbotron .nav-pills').addClass('nav-justified');
+  else
+    $('.jumbotron .nav-pills').removeClass('nav-justified');
 }
